b608c979eeefc02c0496a3136381c80c1ee166df
NgaLeLe <57435241+NgaLeLe@users.noreply.github.com>
2021-09-30 11:37:37 +0200

    dernier affinage le factoryDB.py

--- factoryDB.py
+++ factoryDB.py
@@ -6,8 +6,8 @@ from models import *
 
 db_session = DBsession()
 
-#models.Base.metadata.create_all(engine)
- #DBsession.remove(db_session)
+models.Base.metadata.create_all(engine)
+DBsession.remove(db_session)
 
 
 def insert_db():
@@ -29,16 +29,19 @@ def insert_db():
     books = format_books(books)
     data_users = format_users(ratings)
     ratings = format_ratings(ratings)
+
+    #merger avec books afins d'avoir des ratings concernant des livres existants
     data_ratings = pd.merge( books, ratings, validate="1:m", on="book_id")
     data_ratings = data_ratings[ratings.columns].copy()
     data_ratings = pd.merge(data_users, data_ratings, on="user_id", validate="1:m")
     data_ratings = data_ratings[ratings.columns].copy()
     
+    #affiner des tags, book_tags correspondant les livres exists
     data_tags, data_book_tags = format_tags_booktag(tags, book_tags)
     data_book_tags = pd.merge(books[['goodreads_book_id']], data_book_tags, on='goodreads_book_id' )
     
- 
     to_reads = format_to_read(to_reads)
+    #merger avec books afins d'avoir des book_to_read lié avec des livres existants
     data_toread = pd.merge( books, to_reads, validate="1:m", on="book_id")
     data_toread = data_toread[to_reads.columns].copy()
     data_toread = pd.merge(data_users, data_toread, on="user_id", validate="1:m")
@@ -47,15 +50,16 @@ def insert_db():
 
     #inserer les datas à BD
     print("Inserer data...")
-    Genre.insert_from_pd(genres, db_session)
+    #Genre.insert_from_pd(genres, db_session)
     Book.insert_from_pd(books, db_session)
     User.insert_from_pd(data_users, db_session)
     Rating.insert_from_pd(data_ratings, db_session)
     Tag.insert_from_pd(data_tags, db_session)
     Book_tags.insert_from_pd(data_book_tags,db_session)
- 
     To_read.insert_from_pd(data_toread, db_session)
+ 
     print("Les données sont insérées to DB")
+ 
     db_session.commit()
     
 insert_db()
