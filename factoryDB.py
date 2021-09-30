9572fb08506e8a9f59d619040e0ff5f18dd950c8
NgaLeLe <57435241+NgaLeLe@users.noreply.github.com>
2021-09-30 09:51:06 +0200

    nouveau affinage des factoryDB et service

--- factoryDB.py
+++ factoryDB.py
@@ -18,16 +18,16 @@ def insert_db():
     # On récupère les données du fichier CSV dans un dataframe; ajout de chemin RecoBooks/ si il ne marche pas
     print("Read CSV")
     books = pd.read_csv("data/books.csv")
-    ratings = pd.read_csv("data/ratings.csv")
-    to_reads = pd.read_csv("data/to_read.csv")
+    #ratings = pd.read_csv("data/ratings.csv")
+    #to_reads = pd.read_csv("data/to_read.csv")
     tags = pd.read_csv("data/tags.csv", encoding="UTF-8")
     book_tags = pd.read_csv("data/book_tags.csv")
-    genres = creer_genre()
+    #genres = creer_genre()
 
     #On formater, nettoyer des donnees
     print("Nettoyer data")
     books = format_books(books)
-    data_users = format_users(ratings)
+    #data_users = format_users(ratings)
     #ratings = format_ratings(ratings)
     #data_ratings = pd.merge( books, ratings, validate="1:m", on="book_id")
     #data_ratings = data_ratings[ratings.columns].copy()
@@ -49,7 +49,7 @@ def insert_db():
 
     #inserer les datas à BD
     print("Inserer data...")
-    Genre.insert_from_pd(genres, db_session)
+    #Genre.insert_from_pd(genres, db_session)
     #Book.insert_from_pd(books, db_session)
     #User.insert_from_pd(data_users, db_session)
     #Rating.insert_from_pd(data_ratings, db_session)
